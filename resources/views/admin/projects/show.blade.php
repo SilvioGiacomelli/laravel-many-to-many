b58fd466e96ae95186e70a55c3ef9ec289cbd6f7
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-28 15:32:57 +0200

    [Fatto v-show]

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.admin')
 @section('content')
-    <h1>PROGETTO</h1>
-@endsection
+    <h1>{{ $project->title }}</h1>
+    <div class="card" style="width: 18rem;">
+        <img src="{{ asset('storage/' . $project->image) }}" class="card-img-top" alt="{{ $project->image }}">
+        <div class="card-body">
+            {{-- <p class="card-text">{{ $project->type->title }}</p> --}}
+            <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-primary">Edit</a>
+            <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </div>
+    @endsection
