859da0792f93bf4a57c59e28a6f9c16825bd7dbf
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-28 17:11:54 +0200

    [Inseriti in v-show badge]

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,6 +4,13 @@
     <div class="card" style="width: 18rem;">
         <img src="{{ asset('storage/' . $project->image) }}" class="card-img-top" alt="{{ $project->image }}">
         <div class="card-body">
+            @if ($project->technologies)
+                <p>Technlogies:
+                    @foreach ($project->technologies as $technology)
+                        <span class="badge bg-warning">{{ $technology->title }}</span>
+                    @endforeach
+                </p>
+            @endif
             {{-- <p class="card-text">{{ $project->type->title }}</p> --}}
             <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-primary">Edit</a>
             <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
