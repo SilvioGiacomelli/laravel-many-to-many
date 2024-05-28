d3419f7a6708a68dd8b974b58ae4d58cd27ea5fa
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-28 16:39:37 +0200

    [Creato Seeder]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             TypeTableSeeder::class,
             TechnologyTableSeeder::class,
             ProjectTableSeeder::class,
+            ProjectTechnologyTableSeeder::class,
         ]);
     }
 }
